768e77343f3c284a710f706b0195a2cc4d383b2d
Joydip <joydip.mondal@gmail.com>
2025-05-06 13:13:56 +0530

    day-2 final upload

---
+++ codes/day-2/LibraryDemo/ClientApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using MessengerLibrary;
+
+Console.WriteLine("this is the client who is using MessengerLibrary");
+
+//creating an instance or object of Messanger class
+Messanger messanger = new Messanger();
+
+//calling the instance method of the class using the reference variable and also storing the returned value in a string variable
+string message = messanger.GetMessage("Joydip");
+
+//printing the returned message
+Console.WriteLine(message);
