a8a8a72c0de6de7a1ecb7feb69431db0af32abca
Joydip <joydip.mondal@gmail.com>
2025-05-06 11:21:18 +0530

    .NET execution, assembly, top-level statement etc.

---
+++ codes/day-2/FirstCLIApp/UserInterface.cs
@@ -0,0 +1,13 @@
+﻿// namespace FirstCLIApp
+// {
+//     internal class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello, World!");
+//         }
+//     }
+// }
+Console.WriteLine("Hello, World!");
+Console.WriteLine("press any key to exit the program...");
+Console.ReadKey(true);
