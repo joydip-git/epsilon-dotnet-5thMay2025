96856feae6f26c58ae444292425a38abb32b2898
Joydip <joydip.mondal@gmail.com>
2025-05-20 10:50:47 +0530

    entities created

---
+++ codes/day-10/Epsilon.DotNet.PmsApp/Epsilon.DotNet.PmsApp.Entities/Product.cs
@@ -0,0 +1,44 @@
+﻿namespace Epsilon.DotNet.PmsApp.Entities
+{
+    public class Product
+    {
+        public Product()
+        {
+
+        }
+
+        public Product(int id, string name, string description, decimal price)
+        {
+            ProductId = id;
+            ProductName = name;
+            ProductDescription = description;
+            Price = price;
+        }
+
+        public int ProductId { set; get; }
+
+        //property setter
+        public string ProductName { set; get; } = string.Empty;
+
+        public string ProductDescription { set; get; } = string.Empty;
+
+        public decimal Price { set; get; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj != null && obj is Product other &&
+                   this.ProductId == other.ProductId;
+        }
+
+        public override int GetHashCode()
+        {
+            const int prime = 31;
+            return HashCode.Combine(ProductId) ^ prime;
+        }
+
+        public override string? ToString()
+        {
+            return $"[Id={ProductId}, Name={ProductName}, Description={ProductDescription}, Price={Price}]";
+        }
+    }
+}
