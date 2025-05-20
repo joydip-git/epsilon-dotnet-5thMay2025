cb0eddaf055c373453e08192a027bb8b7132e179
Joydip <joydip.mondal@gmail.com>
2025-05-20 11:16:23 +0530

    repository created

---
+++ codes/day-10/Epsilon.DotNet.PmsApp/Epsilon.DotNet.PmsApp.Repository/ProductDao.cs
@@ -0,0 +1,28 @@
+﻿using Epsilon.DotNet.PmsApp.Entities;
+
+namespace Epsilon.DotNet.PmsApp.Repository
+{
+    public class ProductDao
+    {
+        public List<Product> GetAll() 
+        { 
+            return new List<Product>();
+        }
+        public Product Get(int id) 
+        { 
+            return new Product(); 
+        }
+        public bool Insert(Product p)
+        {
+            return false;
+        }
+        public bool Update(int id, Product p)
+        {
+            return false;
+        }
+        public bool Delete(int id)
+        {
+            return false;
+        }
+    }
+}
