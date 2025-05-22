d877d8b06bdce2b6bd7b2a6eeb6582ace1f411d4
Joydip <joydip.mondal@gmail.com>
2025-05-22 11:08:03 +0530

    delegates and LINQ update

---
+++ codes/day-12/BuiltInDelegates/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+//TaskDel<string> printDel = new TaskDel<string>(Show);
+TaskDel<string> printDel = Show;
+
+//Lambda expression is the syntax to write an anonymous method.
+//before lambda operator (=>) mention the list of arguments and
+//after the lambda operator, create the method body either using expression body syntax or traditional way (using -> { //code })
+TaskDel<int> printNumberDel = (num) => Console.WriteLine(num);
+*/
+Action<string> printDel = Show;
+printDel("joydip");
+
+Predicate<int> isEven = (num) => num % 2 == 0;
+
+Func<int, bool> isOdd = (num) => num % 2 != 0;
+Func<int, int, bool> isGreater = (x, y) => x > y;
+
+//if return value is 0  -> x is equal to y
+//if return value is <0 -> x is less than y
+//if return value is >0 -> x is greater than y
+Func<int, int, int> compare = (x, y) => x - y;
+
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+List<int> result = Filter(numbers, isEven);
+
+//Action<int> printNumber = (num) => Console.WriteLine(num);
+//result.ForEach(printNumber);
+result.ForEach(num => Console.WriteLine(num));
+
+static void Show(string name) => Console.WriteLine(name);
+
+//delegate void TaskDel<in T>(T input);
+//delegate bool LogicInvoker<in T>(T t);
+static List<T> Filter<T>(List<T> input, Predicate<T> logic)
+{
+    var list = new List<T>();
+    foreach (var item in input)
+    {
+        bool isTrue = logic(item);
+        if (isTrue)
+            list.Add(item);
+    }
+    return list;
+}
---
+++ codes/day-12/LINQDemo/Program.cs
@@ -0,0 +1,66 @@
+﻿//source of data
+List<int> numbers = [1, 2, 5, 3, 7, 8, 6, 4, 0, 9];
+List<string> names = ["anil", "sunil", "joydip", "vinod"];
+
+//1. method query syntax
+numbers
+    .OrderBy(x => x)
+    .Where(x => x % 2 == 0)
+    .Take(3)
+    .ToList()
+    .ForEach(x => Console.WriteLine(x));
+
+names
+    .OrderByDescending(name => name)
+    .Where(name => name.Contains('n') || name.Contains('N'))
+    .Take(2)
+    .ToList()
+    .ForEach(name => Console.WriteLine(name));
+
+//2. query (operator) syntax
+var query = from x in numbers
+            orderby x ascending
+            where x % 2 == 0
+            select x;
+
+query
+    .Take(3)
+    .ToList()
+    .ForEach(x => Console.WriteLine(x));
+
+
+
+/*
+//1. sort the numbers in ascending order without mutating the original collection
+//1. sort the names in ascending alphabetical order without mutating the original collection
+
+Func<int, int> orderLogic = (x) => x;
+var sortedNumbers = numbers.OrderBy(orderLogic);
+
+Func<string, string> orderNameLogic = (name) => name;
+var sortedNames = names.OrderBy(orderNameLogic);
+
+//2. filter the even numbers from the sorted numbers
+//2. filter the the sorted names by the names which have a letter 'n' in their name
+
+Func<int, bool> isEvenLogic = num => num % 2 == 0;
+var filteredNumbers = sortedNumbers.Where(isEvenLogic);
+
+Func<string, bool> nameWithN = (name) => name.Contains('n') || name.Contains('N');
+var filteredNames = names.Where(nameWithN);
+
+//3. select the first 3 values from the filtered numbers. if 3 values are not present, then select as many are there.
+//3. select the first 3 values from the filtered names. if 3 values are not present, then select as many are there.
+
+var selectedNumbers = filteredNumbers.Take(3);
+var selectedNames = filteredNames.Take(3);
+
+//4. print the final numbers
+//4. print the final names
+
+Action<int> printNumber = (num) => Console.Write(num);
+selectedNumbers.ToList().ForEach(printNumber);
+
+Action<string> printName = name=> Console.WriteLine(name);
+selectedNames.ToList().ForEach(printName);
+*/
